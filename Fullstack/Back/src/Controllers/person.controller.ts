5b9b34dc26004abb0f274a3c46085eeff0f69f27
m1ca000 <164351016+m1ca000@users.noreply.github.com>
2025-07-15 11:35:51 -0300

    fix: funcion para guardar persona funca
    CLOUDINARY FUNCOOOOOOO

--- Fullstack/Back/src/Controllers/person.controller.ts
+++ Fullstack/Back/src/Controllers/person.controller.ts
@@ -1,9 +1,13 @@
-import { savePersonRegister, getPersonsByInstitution,uploadImage } from "../Services/person.service";
+import { savePersonRegister, getPersonsByInstitution, uploadImage } from "../Services/person.service";
 import { Request, Response } from "express";
 
 export const uploadPerson = async (req: Request, res: Response) => {
     try {
-        const person = await savePersonRegister(req.body.name, req.body.surname, req.body.file.buffer, req.body.institutionID);
+        if (!req.file || !req.file.buffer) {
+            return res.status(400).json({ error: 'No se proporcionó ninguna imagen' });
+        }
+        const imageUrl = await uploadImage(req.file.buffer);
+        const person = await savePersonRegister(req.body.name, req.body.surname, imageUrl, req.body.institutionID);
         
         return res.status(201).json({ message: 'Persona registrada con exito' });
     } catch(err) {
--- Fullstack/Back/src/Services/person.service.ts
+++ Fullstack/Back/src/Services/person.service.ts
@@ -1,6 +1,22 @@
 import { PrismaClient } from '../generated/prisma/client.js';
 const prisma = new PrismaClient();
 import dotenv from 'dotenv';
+import cloudinary from '../Config/cloudinary';
+import { Readable } from 'stream'
+
+export const uploadImage = (fileBuffer: Buffer): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { resource_type: 'image' },
+      (error, result) => {
+        if (error || !result) return reject(error);
+        resolve(result.secure_url);
+      }
+    );
+
+    Readable.from(fileBuffer).pipe(uploadStream);
+  });
+};
 
 export const savePersonRegister = async (name: string, surname: string, photo: string, institutionID: string) => {
   return await prisma.person.create({
